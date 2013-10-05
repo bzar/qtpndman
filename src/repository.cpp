abbbc8a78d22ed37a146d475801687b3b1d55084
Teemu Erkkola <teemu.erkkola@iki.fi>
2013-10-05 13:51:54 +0300

    Don't try to guess whether the package has changed

--- src/repository.cpp
+++ src/repository.cpp
@@ -157,11 +157,8 @@ void QPndman::Repository::update()
       if(package->getId() == packageId)
       {
         newPackage = false;
-        if(x->update || package->getModified() != QDateTime::fromTime_t(x->modified_time))
-        {
-          i.setValue(new Package(context, x, this));
-          package->deleteLater();
-        }
+        i.setValue(new Package(context, x, this));
+        package->deleteLater();
       }
     }
 
